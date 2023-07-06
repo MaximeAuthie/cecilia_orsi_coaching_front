89fba913dc42cf7f5ebfd81b74e467f6ca789c1b
Maxime Authié <authie.maxime@orange.fr>
2023-07-06 17:55:39 +0200

    ajout initial template + script + style

--- components/headerComponent.vue
+++ components/headerComponent.vue
@@ -1,13 +1,235 @@
 <template>
-    <div>
-        This is the header of the application
-    </div>
+    <header>
+        <div class="header_logo">
+            <img src="/logo_header.png" alt="Cécilia Orsi Coaching">
+        </div>
+        <nav :class="actualClass">
+            <div class="header_nav_logo">
+                <img src="/logo_nav_header.png" alt="">
+            </div>
+            <ul class="header_nav_list">
+                <li><nuxt-link @click="openBurgerMenu" to="/" class="header_nav_list_link">Accueil</nuxt-link></li>
+                <li><nuxt-link @click="openBurgerMenu" to="owner" class="header_nav_list_link">Qui suis-je?</nuxt-link></li>
+                <li><nuxt-link @click="openBurgerMenu" to="/prices" class="header_nav_list_link">Tarifs</nuxt-link></li>
+                <li><nuxt-link @click="openBurgerMenu" to="/blog" class="header_nav_list_link">Blog</nuxt-link></li>
+                <li><nuxt-link @click="openBurgerMenu" to="/appointment" class="header_nav_list_link">Prendre rendez-vous</nuxt-link></li>
+                <li><nuxt-link @click="openBurgerMenu" to="/contact" class="header_nav_list_link">Contact</nuxt-link></li>
+            </ul>
+        </nav>
+        <div @click="openBurgerMenu" class="header_burger">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+        </div>
+    </header>
 </template>
 
-<script setup lang="ts">
-
+<script lang="ts">
+   export default {
+    data() {
+        return {
+            isMenuOpen : false,
+            actualClass: 'header_nav',
+            windowWidth: 0
+        }
+    },
+    methods: {
+        openBurgerMenu() {
+            console.log("Menu burger cliqué!");
+            this.isMenuOpen = !this.isMenuOpen;
+            this.windowWidth = window.innerWidth;
+            if (this.windowWidth < 1210) {
+                 if (this.isMenuOpen) {
+                this.actualClass = 'header_nav_open';
+                } else {
+                    this.actualClass = 'header_nav_close';
+                }
+            }
+           
+        }
+    },
+   }
 </script>
 
 <style scoped>
+header {
+    position: sticky;
+    z-index: 10;
+    top: 0px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: calc(100%-4vw);
+    height: 15vh;
+    padding: 0 2vw;
+    box-shadow: 0 4px 2px rgba(0, 0, 0, 0.25);
+}
 
+.header_logo {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    max-width: 90%;
+}
+
+.header_logo img {
+   max-height: 100%;
+   max-width: 100%;
+}
+
+.header_burger {
+    display: block;
+}
+
+.header_burger svg{
+    stroke: #B05447;
+    width: 5vh;
+    height: 5vh;
+}
+.header_nav {
+    display: none;
+}
+
+.header_nav_logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 70%;
+    margin-bottom: 5vh;
+}
+
+.header_nav_logo img {
+    max-width: 90%;
+    height: auto;
+}
+
+.header_nav_close {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: fixed;
+    top: 15vh;
+    right: 0;
+    width: 100%;
+    height: 85vh;
+    background-color: #B05447;
+    animation: closeMenu 300ms ease-in-out forwards;
+}
+
+.header_nav_open {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: fixed;
+    top: 15vh;
+    right: 0;
+    width: 100%;
+    height: 85vh;
+    background-color: #B05447;
+    animation: openMenu 300ms ease-in-out forwards;
+}
+
+.header_nav_list_link {
+    margin-left: 2vw;
+    text-decoration: none;
+    color: #B05447;
+    text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+}
+.header_nav_list_link:visited {
+    color: #B05447;
+}
+.header_nav_list_link:hover {
+    text-decoration: underline;
+}
+
+@keyframes openMenu {
+    0% {
+        transform: translateX(100%);
+    }
+    100% {
+        transform: initial;
+    }
+}
+
+@keyframes closeMenu {
+    100% {
+        transform: translateX(100%);
+    }
+    0% {
+        transform: initial;
+    }
+}
+
+.header_nav_list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'La Belle Aurore', cursive;
+    font-size: 2em;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+}
+
+.header_nav_list_link {
+    margin-left: 0;
+    color: #FFFFFF;
+}
+
+.header_nav_list_link:visited {
+    color: #FFFFFF;
+}
+
+
+@media screen and (min-width: 1210px) {
+    header {
+        max-height: 12vh;
+    }
+    .header_logo {
+        max-width: 30%;
+    }
+    .header_logo img {
+        max-height: 100%;
+        width: auto;
+    }
+    
+    .header_nav {
+        display: flex;
+    }
+
+    .header_nav_logo {
+        display: none;
+    }
+
+    .header_nav_list {
+        flex-direction: row;
+        font-size: 1.5em;
+    }
+
+    .header_nav_list_link {
+        color:#398C7E;
+        margin-left: 2vw;
+    }
+    .header_nav_list_link:visited {
+        color:#398C7E;
+    }
+
+    .header_burger {
+        display: none;
+    }
+}
+
+@media screen and (min-width: 1400px) {
+    .header_nav_list {
+        font-size: 2em;
+    }
+
+    .header_nav_list_link {
+        color:#B05447;
+        margin-left: 3vw;
+    }
+}
 </style>
