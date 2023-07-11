fba9ae3bd73ccd7a77700047c05fc043111fff2f
Maxime Authié <authie.maxime@orange.fr>
2023-07-11 18:19:13 +0200

    Démarrage formulaire

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,24 @@
 <template>
     <BannerComponent :imgUrl="page.bannerUrl" :messages="bannerMessages" :isMainButtonActive="page.isMainButtonActive" :isSecondButtonActive="page.isSecondaryButtonActive" ></BannerComponent>
     <div class="content">
+        <section class="content_form">
+            <h2>Envoyer un message</h2>
+            <form @submit.prevent="">
+                <label for="firstName">Votre prénom</label><br>
+                <input  class="form_input" type="text" name="firstName" id="firstName"><br>
+                <label for="lastName">Votre nom</label><br>
+                <input  class="form_input" type="text" name="lastName" id="lastName"><br>
+                <label for="email">Votre adresse mail</label><br>
+                <input  class="form_input" type="mail" name="email" id="email"><br>
+                <label for="subject">Sujet de votre message</label><br>
+                <input  class="form_input" type="text" name="subject" id="subject"><br>
+                <label for="content">Votre message</label><br>
+                <textarea name="content" id="content">Saisir votre message ici</textarea>
+
+                <input class="button button_form" type="button" value="Envoyer">
+            </form>
+           
+        </section>
         <section class="content_tiles">
             <TileComponent v-for="tile in tilesList" :pageTitle="tile.title" :pagePath="tile.path" :pageImgUrm="tile.imgUrl" ></TileComponent>
         </section>
