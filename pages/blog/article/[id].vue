8a78b42fe3f451ba7e2a3fd9f439218a79d7909c
Maxime Authié <authie.maxime@orange.fr>
2023-07-24 19:08:07 +0200

    Récupération dynamique des données affichées

--- pages/blog/article/[id].vue
+++ pages/blog/article/[id].vue
@@ -1,120 +1,37 @@
 
 <template>
-  <div class="banner" :style="{backgroundImage: articleData.bannerUrl}">
-      <div class="banner_logo">
-          <img src="~/assets/images/logo_nav_header.png" alt="">
-      </div>
-      <div class="banner_title">
-          <h1>{{ articleData.title }}</h1>
-      </div> 
+    <div class="banner" :style="{backgroundImage: articleDataTest.banner_url_article}">
+            <div class="banner_logo">
+            <img src="~/assets/images/logo_nav_header.png" alt="">
+        </div>
+        <div v-if="isArticleLoaded" class="banner_title">
+            <h1>{{ articleDataTest.title_article }}</h1>
+        </div> 
       
-      <div class="banner_informations">
-          Par {{ articleData.user.firstName }} {{ articleData.user.lastName }} - le {{ articleData.date }}
-      </div>
-  </div>
-  <div class="content">
-    <section class="content_article">
-        <p>
-            Pour vivre et se développer, toute entreprise se doit aujourd’hui d’être performante.Dans quelles mesures la performance est-elle liée au stress ?<br>
-
-            Quelle est la relation de l’entreprise face à la performance ? Pendant longtemps, la relation au travail était une relation contractuelle ; bien travailler consistait à faire ce qui était demandé.<br>
-
-            L’agence européenne pour la sécurité et la santé au travail définit le stress comme « Un état qui survient lorsqu’il y a déséquilibre entre la perception d’une personne a des contraintes que lui impose son environnement et la perception de ses propres ressources pour y faire face. »<br>
-
-            Actuellement, faire son travail ne suffit plus, il faut que les salariés se dépassent. L’entreprise veut toujours aller plus loin et ne se contente plus d’un travail bien fait. Elle exige donc toujours plus aux employés, qui deviennent des champions du travail. Il est donc essentiel de trouver un juste milieu quant aux demandes, afin de ne pas entraîner des réactions de stress sans fin qui amoindrirait la performance. <br>
-        </p>
-        <p>
-            Quelques facteurs générateurs de stress dans une entreprise :<br>
-            <ul>
-                <li> Le contenu du travail à effectuer</li>
-                <li> Les exigences des supérieurs</li>
-                <li> Les facteurs liés à l’organisation du travail, aux relations de travail</li>
-                <li> L’incertitude de l’avenir selon les types de contrats et selon l’entreprise…</li>
-            </ul>
-        </p>    
-        <p>Ces différents facteurs n’ont pas les mêmes prises ainsi que les mêmes répercussions en fonction de chaque individu. Face à une situation identique, le stress peut prendre une apparence et un degré d’intensité différent d’un individu à l’autre. Certaines entreprises utilisent le stress comme moteur de performance. Nous allons voir ici comment il peut paraître « positif » sur le plan de la productivité. Mais aussi, à l’inverse, se montrer contre-productif et même parfois dangereux d’un point de vue physique et psychologique, notamment lorsque celui-ci est récurrent voir chronique et qu’il submerge l’individu.</p>
-        <h3>1. Le stress comme “performer”</h3>
-        <p>
-            Chez certaines personnes et en ce qui concerne un stress de faible intensité et de courte durée, il peut s’avérer qu’il devienne salutaire. Le stress peut être une réaction de l’organisme pour s’adapter à son environnement, cela les rend plus performants et plus efficaces. <br>
-
-            Les travaux du toxicologue allemand Hugo Schultz montrèrent dès 1888 que de petites doses de poison stimulaient la croissance de levures alors que des doses plus importantes l’altéraient. Ainsi, l’exposition à de faibles doses de phénomènes générateurs de stress pourrait s’avérer bénéfique. Plus on monte dans la pyramide hiérarchique, plus les individus sont stressés et deviennent les principaux « agents stresseurs » à même de soumettre leurs collaborateurs au stress. <br>
-
-            C’est peut-être dans la capacité des entrepreneurs à diminuer leur stress post création, et aux dirigeants à ne surtout pas le transmettre au reste de l’organisation, que résiderait la clé d’une performance durable. <br>
-
-            Certains salariés se sentent « portés » par le stress, cela les motive et leur donne une raison de se dépasser qui leur apportera par la suite, une grande satisfaction d’eux-mêmes et de leurs travaux accomplis. <br>
-
-            En témoignent les sportifs de haut niveau. Ils réussissent à exploiter positivement le stress. Par ailleurs, lors d’un entretien professionnel, le stress aide certains à mieux se concentrer et à être plus réactif. Certaines personnes arrivent à gérer le stress voir le réduire grâce notamment à l’alimentation, au sport et au repos. <br>
-
-            La méditation semble, pour certain, adéquat pour gérer un stress et revenir à une paix intérieure plus sereine, sans être débordé par les pensées négatives crées par la situation stressante. <br>
-        </p>
-
-        <h3>2. Les effets négatifs du stress au travail</h3>
-            <p>
-                Longtemps considéré comme carburant pour atteindre des sommets de performance, les experts du monde entier s’accordent aujourd’hui pour dire que cette approche est un leurre. Une nouvelle fois, comme à l’égard de tant d’autres choses, l’Homme a détourné un processus bénéfique et l’a dénaturé pour l’utiliser à mauvais escient. <br>
-
-                Après des rappels scientifiques et historiques sur les origines du Stress arrive l’éclairage économique. Frénésie du chiffre à tout va, volonté totalitaire de contrôle sont des notions nouvelles qui expliquent comment notre société en est arrivée à ce niveau de souffrance et de dérèglement. La corrélation entre course à l’argent effrénée, prise d’anxiolytiques et mal être au travail est aujourd’hui scientifiquement prouvée. <br>
-
-                Un professeur, Philippe Rodet rappelle que la raison du stress est hormonale. L’adrénaline produite par le stress provoque la baisse de l’acétylcholine indispensable à la motivation. Le stress chronique peut conduire au Burn out. Les personnes souffrent alors de différents maux et entre autres nous y retrouverons :
-                
-                <ul>
-                    <li>Déconcentration</li>
-                    <li>Des maux de tête</li>
-                    <li>De l’anxiété</li>
-                    <li>Des troubles digestifs</li>
-                    <li>Un sommeil difficile</li>
-                    <li>De l’irritabilité</li>
-                    <li>Des douleurs chroniques</li>
-                </ul>
-            </p>
-            <p>
-                L’entreprise utilisant le stress comme méthode de management se pénalise elle-même à long terme, elle perd l’efficacité de ses employés. En plus de perdre ses ressources humaines, elle perd également de l’argent. Celle-ci exerce une pression écrasante sur les individus qui se retrouvent en rupture de ban, exclut de l’organisation. Cela engendre un coût supplémentaire pour l’entreprise. <br>
-
-                L’arrêt de travail, le recrutement pour un remplacement, la formation d’un nouvel arrivant coûte du temps et de l’argent donc un manque à gagner considérable pour la firme. Une mauvaise ambiance peut s’installer entre les dirigeants et les employés. <br>
-                L’individu malade, dû à un surmenage professionnel n’est plus le « meilleur ». Celui-ci est une fois de plus déconsidérer, il devient un perdant aux yeux de la société. Notre société du succès ne connaît que les gagnants et n’a que faire des perdants. Le stress de la performance implique une rupture avec la structure et, aussi une rupture relationnelle. <br>
-
-                L’estime de soi est souffreteuse, l’individu est entraîné dans un circuit de stress. Après s’être investit jusqu’à l’épuisement pour une structure, elle met de côté l’individu. <br>
-                <br>
-                <strong>a. Se réorganiser</strong> <br>
-                <br>
-                Il est possible, pour réduire le stress au travail, de trouver des « aides » comme par exemple la ré-organisation. Il est possible d’organiser nos tâches en utilisant la matrice d’Eisenhower. La matrice d’Eisenhower est un outil d’organisation efficace pour trier nos tâches selon leur importance et leur urgence. <br>
-
-                Une tâche importante est une tâche qui contribue à accomplir nos objectifs de long terme. Une tâche urgente est une tâche qui doit être traitée immédiatement. Celle qui est importante n’est pas forcément urgente et inversement. Pour prioriser il est important de s’attaquer d’abord aux tâches importantes et urgentes puis aux tâches importantes mais non urgentes. Pour les tâches urgentes et non importantes, il faut les déléguer autant que possible.<br>
-                <br>
-                <strong>b. Apprendre à dire non</strong> <br>
-                <br>
-                Quand quelqu’un nous demande un service, nous sommes souvent tentés de dire oui. Soit parce que nous voulons rendre service, soit parce que nous avons peur de vexer. Seulement nous oublions trop souvent que dire oui, c’est s’engager. C’est accepter d’investir notre temps et notre énergie pour rendre service. Et plus nous disons oui, plus nous engageons notre temps et notre énergie. Jusqu’au moment où nous sommes engagés sur tellement de choses que nous nous sentons complètement submergés. Dire non nous permet d’éviter la surcharge de travail. En travaillant sur moins de choses, nous restons en contrôle et nous pouvons travailler sans stress. <br>
-                <br>
-                <strong>c. Changer son environnement de travail</strong> <br>
-                <br>
-                Notre environnement de travail impacte énormément notre niveau de stress. Un espace bruyant, un bureau mal situé ou encore un manque de luminosité sont autant d’éléments qui peuvent amplifier notre stress au travail. <br>
-                Rangez et optimisez votre espace de travail,décorez votre bureau pour le rendre plus chaleureux par exemple paraît n’être pas grand-chose mais contribue au bien être au travail, surtout lorsque vous y passer 8h par jour ! <br>
-                <br>
-                <strong>d. Régler les conflits</strong> <br>
-                <br>
-                Les conflits au travail sont une grande source de stress et il est important de les régler le plus rapidement possible. L’erreur serait de ne rien faire en attendant que les choses se tassent. La plupart des conflits peuvent être régler et faire appel à un coach peut aider en ce sens . La communication non violente, mise en avant par Marshall Rosenberg permet entre autre, de pouvoir se faire comprendre et mieux communiquer envers ses employés, ses collègues, ses supérieurs… Les coachings en entreprise sont clairement bénéfiques à l’entreprise et aux personnes y travaillant ; ils permettent à tout le monde de s’y retrouver, chef d’entreprise, managers, employés etc… <br>
-                <br>
-                Pour conclure, l’entreprise doit concilier performance économique et équilibre individuel et mettre en place des systèmes de gestion qui tiennent compte de cette double exigence. <br>
-                L’individu est capable de gérer la pression à court terme mais il éprouve des difficultés face à une exposition prolongée ou répétées à des pressions intenses. <br>
-                Au travers de ces définitions, nous observons que le stress n’est pas une maladie mais plus un phénomène subjectif, avec de possibles répercussions physiques et psychiques. <br>
-                Dans notre société actuelle, les esprits s’éveillent à un nouveau management, un management plus respectueux des salariés. <br>
-                Notamment parce qu’on s’est rendu compte (grâce au psychologue Elton Mayo) que de bonnes conditions de travail favorise la productivité des employés.De ce fait, certaines entreprises (telles que ben&jersry’s pour citer un exemple) mettent tout en œuvre pour favoriser le bien-être de ses employés afin qu’ils s’y sentent bien et trouvent ses besoins comblés. <br>
-                <br>
-                Je conclurai donc que le stress, de courte durée peut être un moteur de performance pour certaines personnes. Cependant, nous pouvons affirmer qu’un stress prolongé est mauvais, et pour la personne qui le vît, et pour l’entreprise à long terme.Il est donc important que les entreprises prennent conscience des besoins des salariés, adopte un management plus respectueux des besoins de ceux-ci. Les dirigeants, les patrons, doivent se former et être eux-mêmes suivis, pour gérer leurs stress et éviter de le propager dans l’entreprise. Les employés quant à eux peuvent gérer certains stress grâce à des « méthodes » et /ou des suivis de « coaching » ou « psychologiques ». <br>
-            </p>
-            
+        <div v-if="isArticleLoaded" class="banner_informations">
+            Par {{ articleDataTest.user.first_name_user }} {{ articleDataTest.user.last_name_user }} - le {{ articleDataTest.date_article }}
+        </div>
+    </div>
+    <div class="content">
+    <section v-html="articleDataTest.content_article" class="content_article">
+       
     </section>
 
     <section class="content_comments">
-        <CommentsSpaceComponent :comments="articleData.comments"></CommentsSpaceComponent>
+        <CommentsSpaceComponent :articleId="id" :comments="comments"></CommentsSpaceComponent>
     </section>
   </div>
 </template>
 
-<script lang="ts">
+<script>
+
+    import { useArticlesStore } from '@/store/article';
+    import { useCommentsStore } from '@/store/comment'
+
     export default {
         data() {
             return {
-                id: '' as string,
+                id: '',
                 articleData: {
                     id: 7,
                     title: 'Stress et performance en entreprise',
@@ -137,26 +54,71 @@
                             color:  "#8EBBA7"
                         },
                     ],
-                    comments: [
-                        {
-                            id: 1,
-                            authorName :    'Anne-Marie GERARD',
-                            date:           '21/06/2023 08:30',
-                            content:        "Merci Cécilia pour ce nouvel article de qualité! :)"
-                        },
-                        {
-                            id: 2,
-                            authorName :    'Cécilia Orsi',
-                            date:           '21/06/2023 09:30',
-                            content:        "Merci beaucoup Anne-Marie! N’hésitez pas à me suggérer d’autres sujets pour de prochaines articles!"
-                        }
-                    ]
+                },
+                articleDataTest: {},
+                isArticleLoaded :  false,
+                comments: []
+            }
+        },
+        methods: {
+            getArticleData() {
+                const store = useArticlesStore();
+
+                //? Vérifier si les articles sont toujours présents dans le store, récupérer les données de l'article
+                if (store.articles.length > 0) {
+                    this.articleDataTest            = store.articles.find( article => article.id == this.id);
+                    this.isArticleLoaded            = true;
+                } else {
+
+                //? Si les articles ne sont pas déjà présents dans le store, effectuer l'appel API et récupérer les données de l'article
+                store.getAllArticles()
+                    .then(() => {
+                    this.articleDataTest            = store.articles.find( article => article.id == this.id);
+                    this.isArticleLoaded            = true;
+                    })
+
+                    //? En cas d'erreur inattendue, capter l'erreur rencontrée
+                    .catch((error) => {
+                    console.error('Erreur lors de la récupération des articles :', error);
+                    this.loading                    = false;
+                    });
+                }
+
+            },
+            getComments() {
+               const store = useCommentsStore();
+
+               //? Vérifier si les articles sont toujours présents dans le store, récupérer les données de l'article
+               if (store.comments.length > 0) {
+                    this.comments                   = store.comments.filter( comment => comment.article.id == this.id);
+                    this.isArticleLoaded            = true;
+                } else {
+
+                //? Si les articles ne sont pas déjà présents dans le store, effectuer l'appel API et récupérer les données de l'article
+                store.getAllComments()
+                    .then(() => {
+                    this.comments            = store.comments.filter( comment => comment.article.id == this.id);;
+                    this.isArticleLoaded            = true;
+                    })
+
+                    //? En cas d'erreur inattendue, capter l'erreur rencontrée
+                    .catch((error) => {
+                    console.error('Erreur lors de la récupération des articles :', error);
+                    this.loading            = false;
+                    });
                 }
             }
         },
         mounted() {
+
+            //? Récupération de l'id de l'article dans la route à l'ouverture de la page
             this.id = this.$route.params.id.toString();
 
+            //? Récupérer les information de l'article concerné dans le store Article
+            this.getArticleData();
+            this.getComments();
+
+            //? Renseigner les balises HTML de <head> pour le SEO
             useHead({
                 title: 'Cécilia Orsi Coaching - ' + this.articleData.title,
                 meta: [
