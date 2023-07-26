a695d8bb46e2655c8b4eceb17d5b639ed054fa15
Maxime Authié <authie.maxime@orange.fr>
2023-07-26 18:29:35 +0200

    création layout admin

---
+++ layouts/admin.vue
@@ -0,0 +1,33 @@
+<template>
+    <ManagerHeaderComponent></ManagerHeaderComponent>
+    <ManagerNavigationComponent :route="route"></ManagerNavigationComponent>
+    <slot />
+</template>
+<script>
+    import { useCommentsStore } from '@/store/comment';
+
+    export default {
+        setup() {
+            const commentsStore = useCommentsStore();
+            commentsStore.getCommentsToValidate();
+        },
+        data() {
+            return {
+                route: ''
+            }
+        },
+        updated() {
+            this.route = this.$route.path;
+        },
+        mounted() {
+            this.route = this.$route.path;
+        },
+    }
+</script>
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #4B453F;
+    }
+    
+</style>
