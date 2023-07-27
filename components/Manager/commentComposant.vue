38c2635ed18babe19d586fb3920ca89a57b96329
Maxime Authié <authie.maxime@orange.fr>
2023-07-27 20:12:44 +0200

    rendu dynamique + méthodes + subscribe state

--- components/Manager/commentComposant.vue
+++ components/Manager/commentComposant.vue
@@ -1,40 +1,114 @@
 <template>
     <div class="comment_card">
         <div class="comment_card_header">
-            Article : Notre rapport à la nature
+            Article : {{ title }}
         </div>
         <div class="comments_card_content">
             <div class="comments_card_content_top">
                 <div class="comments_card_content_top_informations">
-                    De Josiane Ducoin - le 27/06/2022 à 19:55
+                    De {{ author }} - le {{ dateTime }}
                 </div>
                 <div class="comments_card_content_top_validation">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#15ba52" class="w-6 h-6" >
+                    <svg @click="validateComment" class="comments_card_content_top_validation_validate" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#15ba52" >
                         <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#E04F5F" class="w-6 h-6">
+                    <svg @click="rejectComment" class="comments_card_content_top_validation_reject" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#E04F5F">
                         <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" />
                     </svg>
                 </div>
             </div>
             <div class="comments_card_content_bottom">
-                Je trouve le contenu de votre article complètement absurde. Mais comme on dit, il en faut pour tous les goûts!!! :/
+                {{ content }}
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import { useCommentsStore } from '@/store/comment';
 
-export default {
-    data() {
-        return {
-            comments: []
+    export default {
+        props: {
+            id: {
+                type:       Number,
+                required:   true
+            },
+            title : {
+                type:       String,
+                required:   true
+            },
+            author : {
+                type:       String,
+                required:   true
+            },
+            dateTime : {
+                type:       String,
+                required:   true
+            },
+            content : {
+                type:       String,
+                required:   true
+            }
+        },
+        data() {
+            return {
+                comments: []
+            }
+        },
+        methods: {
+            async validateComment() {
+
+                let body = {
+                    commentId:  this.id,
+                    userId:     1
+                }
+
+                body = JSON.stringify(body);
+                console.log(body);
+                await fetch('https://127.0.0.1:8000/api/comment/validate', {
+                        method:'PATCH',
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json",
+                            "Access-Control-Allow-Origin": "*"
+                        },
+                        body: body
+                    })
+                    .then(async response => {
+                        this.updateStore();
+                    })
+
+            },
+            async rejectComment() {
+
+                let body = {
+                    commentId:  this.id,
+                    userId:     1
+                }
+
+                body = JSON.stringify(body);
+                console.log(body);
+                await fetch('https://127.0.0.1:8000/api/comment/reject', {
+                        method:'PATCH',
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json",
+                            "Access-Control-Allow-Origin": "*"
+                        },
+                        body: body
+                    })
+                    .then(async response => {
+                        this.updateStore();
+                    })
+
+            },
+            updateStore() {
+                const store = useCommentsStore();
+                store.getCommentsToValidate();
+                store.getValidatedComments();
+            }
         }
-    },
-    methods: {
     }
-}
 </script>
 <style scoped>
     .comment_card {
@@ -66,8 +140,8 @@ export default {
         font-size: 3em;
     }
     .comments_card_content svg {
-        width: 4vh;
-        height: 4vh;
+        width: 5vh;
+        height: 5vh;
     }
 
     .comments_card_content_top {
@@ -77,13 +151,21 @@ export default {
         align-items: center;
         width: 95%;
         padding-top: 1vh;
-        font-size: 0.5em;
+        font-size: 0.55em;
         font-weight: bold;
     }
 
     .comments_card_content_bottom {
         width: 95%;
         font-size: 0.5em;
-        margin: 3vh 0;
+        margin: 2vh 0;
+    }
+
+    .comments_card_content_top_validation_validate {
+        cursor: pointer;
+    }
+
+    .comments_card_content_top_validation_reject {
+        cursor: pointer;
     }
 </style>
